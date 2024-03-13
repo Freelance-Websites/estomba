d1bae9b79461ee0b0ef6fb0c04fa74c60415def1
Juan Martín García <hi@juanmartingarcia.com>
2024-03-13 19:36:06 -0300

    Basic hero animation and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import { attributes } from '@/src/content/index.md';
+import { attributes } from '@/content/index.md';
+import Hero from '@/components/Hero';
+
+interface Section {
+  type: string;
+  image?: string;
+  title?: string;
+}
 
 export default function Home() {
   return (
-   <p>Hello, world</p>
+   <main>
+    {attributes.sections.map((section: Section, index: Number) => {
+      switch(section.type) {
+        case 'hero':
+          return (
+            <Hero
+              image={section.image || ""}
+              title={section.title || "estomba"}
+            />
+          )
+        default:
+          break;
+      }
+    })}
+   </main>
   );
 }
