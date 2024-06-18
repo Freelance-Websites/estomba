5baae8112cf472084f2ec127622da53fe6614a2d
Juan Martín García <hi@juanmartingarcia.com>
2024-06-18 16:59:04 -0300

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,23 @@ export default function RootLayout({
       className={`${GeistSans.className} overflow-x-hidden`}
     >
       <head>
-        <meta name="title" content="Estomba" />
+        <title>Estomba</title>
         <meta name="description" content="Una propuesta de inversión, que combina un edificio de vivienda de pocas unidades en armonía con un barrio de baja densidad, donde predominan las casas." />
+
+        <meta itemprop="name" content="Estomba" />
+        <meta itemprop="description" content="Una propuesta de inversión, que combina un edificio de vivienda de pocas unidades en armonía con un barrio de baja densidad, donde predominan las casas." />
+        <meta itemprop="image" content="https://estomba.com.ar/images/grid-3.jpg" />
+
+        <meta property="og:url" content="https://estomba.com.ar" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Estomba" />
+        <meta property="og:description" content="Una propuesta de inversión, que combina un edificio de vivienda de pocas unidades en armonía con un barrio de baja densidad, donde predominan las casas." />
+        <meta property="og:image" content="https://estomba.com.ar/images/grid-3.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Estomba" />
+        <meta name="twitter:description" content="Una propuesta de inversión, que combina un edificio de vivienda de pocas unidades en armonía con un barrio de baja densidad, donde predominan las casas." />
+        <meta name="twitter:image" content="https://estomba.com.ar/images/grid-3.jpg" />
       </head>
       <body className="antialiased selection:bg-black selection:text-white overflow-x-hidden">{children}</body>
     </html>
